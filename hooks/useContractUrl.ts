d6f498534068ed33ab124792470194009c62c7ca
holy-pengy <holypengy@gmail.com>
2023-10-31 11:34:18 +1000

    feat: add default explorer url

--- hooks/useContractUrl.ts
+++ hooks/useContractUrl.ts
@@ -3,10 +3,14 @@ import { useNetwork } from "wagmi";
 
 export const useContractUrl = (address: string) => {
   const { chain } = useNetwork();
-  return chain?.blockExplorers?.default.url + "/address/" + address;
+  const explorerLink =
+    chain?.blockExplorers?.default.url || "https://etherscan.io";
+  return explorerLink + "/address/" + address;
 };
 
 export const useTxUrl = (hash: Hash) => {
   const { chain } = useNetwork();
-  return chain?.blockExplorers?.default.url + "/tx/" + hash;
+  const explorerLink =
+    chain?.blockExplorers?.default.url || "https://etherscan.io";
+  return explorerLink + "/tx/" + hash;
 };
