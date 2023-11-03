ff23e49f564b52b0803baef4dd60d39ab60f205d
holy-pengy <holypengy@gmail.com>
2023-11-03 22:43:41 +1000

    feat: update mobile menu visibility

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -46,7 +46,7 @@ export default function Navbar() {
           <WalletConnect />
         </div>
 
-        <aside className="flex w-10 h-10 xl:hidden">
+        <aside className="flex w-10 h-10 md:hidden">
           <div className="flex items-center">
             <label
               className="absolute z-[9999] cursor-pointer px-3 py-6"
